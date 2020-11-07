c5be7121cc066f6daabc87337762a12decdd0816
Jeffrey King <jek582@mail.harvard.edu>
2020-11-06 20:20:10 -0500

    updates to visualize page

--- pages/visualize/index.tsx
+++ pages/visualize/index.tsx
@@ -4,24 +4,52 @@ import { UserMatchesContext } from "context/UserMatchesProvider";
 import { getActivitiesByMatchType } from "components/matchVisualizations/utils";
 import Sankey from "components/matchVisualizations/Sankey/Sankey";
 import visualizeStyles from "./styles.module.scss";
+import MatchesDropZone from "components/matchesDropZone/MatchesDropZone";
+
 const Visualize = () => {
   const { data, setData } = useContext(UserMatchesContext);
 
   const activityMap = getActivitiesByMatchType(data);
-
-  if (!data.length) {
-    return (
-      <div className={styles.container}>
-        <h1>No Data Yet</h1>
-      </div>
-    );
-  }
-  return (
+  const {
+    potentialMatches,
+    actualMatches,
+    sentLikesWithConversation,
+    receivedLikesWithConversation,
+    sentLikesWithMeeting,
+    receivedLikesWithMeeting,
+  } = activityMap;
+  const noDataComponent = (
     <div className={styles.container}>
-      <h1>Your Data</h1>
+      <h1>Visualize your data</h1>
+      <MatchesDropZone />
+    </div>
+  );
+
+  const dataComponent = (
+    <div className={styles.container}>
+      <h1>Summary</h1>
+      <div>Total Activity: {potentialMatches.length} </div>
+      <div>Matches: {actualMatches.length} </div>
+      <div>
+        Conversations:{" "}
+        {sentLikesWithConversation.length +
+          receivedLikesWithConversation.length}
+      </div>
+      <div>
+        Dates: {sentLikesWithMeeting.length + receivedLikesWithMeeting.length}
+      </div>
+      <h1>Activity Flow</h1>
       <div className={visualizeStyles.sankeyWrapper}>
         <Sankey activityMap={activityMap} />
       </div>
+    </div>
+  );
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        {data.length ? dataComponent : noDataComponent}
+      </main>
     </div>
   );
 };
