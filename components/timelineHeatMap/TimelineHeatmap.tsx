8a11f3a517cf07f9c162961d5ffefce1452e8b4e
Jeffrey King <jek582@mail.harvard.edu>
2020-12-12 11:38:31 -0500

    add timeline header

--- components/timelineHeatMap/TimelineHeatmap.tsx
+++ components/timelineHeatMap/TimelineHeatmap.tsx
@@ -1,16 +1,25 @@
 import { DataPoint } from "./types";
 import { formatData, getColorScaler } from "./utils";
+import moment from "moment";
 
 type Props = {
   data: DataPoint[];
   getColor?: (value: number) => string;
   startDate: Date;
   endDate: Date;
+  // Not sure if this is the best
+  isHeaderRow?: boolean;
 };
 
 const defaultGetColor = getColorScaler(10);
 
-const TimelineHeatMap = ({ data, getColor, startDate, endDate }: Props) => {
+const TimelineHeatMap = ({
+  data,
+  getColor,
+  startDate,
+  endDate,
+  isHeaderRow,
+}: Props) => {
   const getColorActual = getColor || defaultGetColor;
   const months = formatData(data, {
     startDate,
@@ -18,21 +27,34 @@ const TimelineHeatMap = ({ data, getColor, startDate, endDate }: Props) => {
   });
 
   const side = 25;
+  const getX = (index: number) => side * index + 2 * index;
   return (
     <svg width={400} height={25}>
-      {months.map((month, index) => {
-        return (
-          <rect
-            key={month.monthDate}
-            fill={getColorActual(month.value)}
-            width={side}
-            height={side}
-            x={side * index + 2 * index}
-            strokeWidth={1}
-            stroke="gray"
-          ></rect>
-        );
-      })}
+      {!isHeaderRow &&
+        months.map((month, index) => {
+          return (
+            <rect
+              key={month.monthDate}
+              fill={getColorActual(month.value)}
+              width={side}
+              height={side}
+              x={getX(index)}
+              strokeWidth={1}
+              stroke="gray"
+            ></rect>
+          );
+        })}
+      {
+        // TODO: A hack - not an ideal way to call for the header row, but key is to keep calculations consistent. Long term a hook might be best to get the x, width, height that can be used across the actual heatmap and the header
+      }
+      {isHeaderRow &&
+        months.map((month, index) => {
+          return (
+            <text x={getX(index)} y={20} fontSize={12}>
+              {moment(month.monthDate).format("MMM")}
+            </text>
+          );
+        })}
     </svg>
   );
 };
--- features/spotify/components/ArtistMonthsHeatMap.tsx
+++ features/spotify/components/ArtistMonthsHeatMap.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import TimelineHeatmap from "components/timelineHeatMap/TimelineHeatmap";
 import styles from "./ArtistMonthsHeatMap.module.scss";
-import { getColorScaler, getStartAndEndDate } from "features/spotify/utils";
+import { getColorScaler } from "features/spotify/utils";
+import moment from "moment";
 
 const ArtistMonthsHeatMap = ({ artists, startDate, endDate }) => {
   const colorScaler = getColorScaler(artists);
@@ -14,21 +15,42 @@ const ArtistMonthsHeatMap = ({ artists, startDate, endDate }) => {
     return colorScaler(value);
   };
 
+  const titleDate =
+    startDate &&
+    endDate &&
+    `${moment(startDate).format("MMM-YYYY")} - ${moment(endDate).format(
+      "MMM-YYYY"
+    )}`;
+
   return (
     <div className={styles.container}>
-      {artists.map((artist, index) => {
-        return (
-          <React.Fragment key={`${artist.artistName}-${index}`}>
-            <div className={styles.artistText}>{artist.artistName}</div>
-            <TimelineHeatmap
-              data={artist.formattedStreams}
-              getColor={getColor}
-              startDate={startDate}
-              endDate={endDate}
-            />
-          </React.Fragment>
-        );
-      })}
+      <h3>Artist Play Frequency by Month ({titleDate})</h3>
+      <div className={styles.chartContainer}>
+        {
+          // blank div for grid
+        }
+        <div></div>
+
+        <TimelineHeatmap
+          data={artists.formattedStreams}
+          startDate={startDate}
+          endDate={endDate}
+          isHeaderRow
+        />
+        {artists.map((artist, index) => {
+          return (
+            <React.Fragment key={`${artist.artistName}-${index}`}>
+              <div className={styles.artistText}>{artist.artistName}</div>
+              <TimelineHeatmap
+                data={artist.formattedStreams}
+                getColor={getColor}
+                startDate={startDate}
+                endDate={endDate}
+              />
+            </React.Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 };
